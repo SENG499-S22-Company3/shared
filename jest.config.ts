fd94570dcef18db9173d9ccc724ee6a3354f68c4
Aomi Jokoji <aomi@uvic.ca>
2022-06-17 10:27:09 -0700

    fix: add c3 as ignore

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,9 @@
 import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
-	preset: "ts-jest",
-	testEnvironment: "node",
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["/node_modules/", "/dist/", "/c3/"],
 };
 
 export default config;
