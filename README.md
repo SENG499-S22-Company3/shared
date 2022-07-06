2e4be955b0ab7545f1837880eedfa509ffca2b51
Aomi <aomi@users.noreply.github.com>
2022-07-05 21:29:34 -0700

    Developer quality of life improvements (#11)
    * chore: format schema
    
    * fix: add missing db-data to gitignore
    
    * chore: move test related to devDependencies
    
    * feat: add graphql linting
    
    * feat(docs): add graphql documentation generation
    
    * refactor: tests
    
    * feat: pull request template for schema changes
    
    * feat(docs): add links to documentation
    
    * feat: CODEOWNERS
    
    * fix: CODEOWNERS syntax
    
    * Revert "fix: CODEOWNERS syntax"
    
    This reverts commit 642cee9241bc98dbe5dae3243f81d0f25d74d6c7.
    
    * feat: use apollo client with cookies
    
    * fix: disable jestopenapi

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# shared
+# SENG 499 Shared for Company 3/4
+
+## Documentation
+
+**[GraphQL](https://seng499-s22-company3.github.io/shared/graphql)** (Backend/Frontend - GraphQL)
+
+**[Algorithm 1](https://seng499-s22-company3.github.io/shared/algorithm1/)** (OpenAPI 3)
+
+**[Algorithm 2](https://seng499-s22-company3.github.io/shared/algorithm2/)** (OpenAPI 3)
--- tests/healthcheck.spec.ts
+++ tests/healthcheck.spec.ts
@@ -1,70 +1,25 @@
-import supertest from "supertest";
-import {
-    gql,
-} from "@apollo/client/core";
-import {
-    BACKEND_URL,
-    ALGORITHM1_URL,
-    ALGORITHM2_URL,
-    apollo_client,
-} from "./test_config";
-
-// Setup supertest for requests to each module
-const request_backend = supertest(BACKEND_URL);
-const request_algorithm1 = supertest(ALGORITHM1_URL);
-const request_algorithm2 = supertest(ALGORITHM2_URL);
+import { request } from "../config";
 
 describe("Algorithm 1 healthchecks", () => {
-    it("should return a running message", async () => {
-        const response = await request_algorithm1.get("/");
-        expect(response.statusCode).toBe(200);
-        expect(response.text).toEqual("Algorithm 1 REST server is alive!");
-    });
+  it("should return a running message", async () => {
+    const response = await request.algorithm1.get("/");
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toEqual("Algorithm 1 REST server is alive!");
+  });
 });
 
 describe("Algorithm 2 healthchecks", () => {
-    it("should return a running message", async () => {
-        const response = await request_algorithm2.get("/healthcheck");
-        expect(response.statusCode).toBe(200);
-        expect(response.text).toEqual("OK");
-    });
-})
+  it("should return a running message", async () => {
+    const response = await request.algorithm2.get("/healthcheck");
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toEqual("OK");
+  });
+});
 
 describe("Backend healthchecks", () => {
-    it("should return a running message", async () => {
-        const response = await request_backend.get("/healthcheck");
-        expect(response.statusCode).toBe(200);
-        expect(response.text).toEqual("OK");
-    });
-
-    it("should return a null result for the GraphQL 'me' query", async () => {
-        const query = gql`
-          query {
-            me {
-              id
-            }
-          }
-        `;
-        const response = await apollo_client.query({ query });
-        expect(response.error).toBeUndefined();
-        expect(response.data.me).toBe(null);        
-    });
-
-    it("should fail login with invalid credentials", async () => {
-      const mutation = gql`
-        mutation {
-          login(username: "test", password: "test") {
-            message
-            token
-            success
-          }
-        }
-      `;
-  
-      const response = await apollo_client.mutate({ mutation });
-      expect(response.errors).toBeUndefined();
-      expect(response.data.login.success).toBe(false);
-      expect(response.data.login.message).toBeDefined();
-      expect(response.data.login.token).toBeDefined();    
-    });
+  it("should return a running message", async () => {
+    const response = await request.backend.get("/healthcheck");
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toEqual("OK");
+  });
 });
