ebbe6e7ce3fc41571f0d3dd39a5c81bcb9f40ba2
Aomi Jokoji <aomi@uvic.ca>
2022-07-05 19:36:25 -0700

    fix: disable jestopenapi

--- tests/algorithm1/schedule.test.ts
+++ tests/algorithm1/schedule.test.ts
@@ -4,4 +4,4 @@ import path from "path";
 const specPath = "./algorithm1/algorithm-1-2.0-resolved.json";
 
 // Load an OpenAPI file (YAML or JSON) into this plugin
-jestOpenAPI(path.join(__dirname, specPath));
+// jestOpenAPI(path.join(__dirname, specPath));
