1fe3cfb2af8c9da5f09650dcf05e47d96e528873
Aomi Jokoji <aomi@uvic.ca>
2022-07-12 13:25:33 -0700

    fix: typo

--- algorithm1/generate.test.ts
+++ algorithm1/generate.test.ts
@@ -3,7 +3,7 @@ import input from "./data/algo1-test-input.json";
 
 describe("algorithm 1", () => {
   it("works for min input", async () => {
-    const res = await request.algorithm1.post("/scheudle").send(input);
+    const res = await request.algorithm1.post("/schedule").send(input);
     expect(res.statusCode).toBe(200);
   });
 });
