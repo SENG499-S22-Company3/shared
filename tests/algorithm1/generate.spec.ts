633d445f3e21fcc8b0ba643984d320a0d9932518
Ben Mazerolle <ben@Bens-MacBook-Pro.local>
2022-07-14 00:30:09 -0700

    adding alg 1 tests, restructure

---
+++ tests/algorithm1/generate.spec.ts
@@ -0,0 +1,27 @@
+import { request } from "../../config";
+
+describe("schedule generation", () => {
+    describe("when an empty course list is passed", () => {
+      it("a schedule is not generated", async () => {
+        const response = await request.algorithm1
+          .post("/schedule")
+          .send([
+            {
+                hardScheduled: {
+                    fallCourses: [],
+                    springCourses: [],
+                    summerCourses: []
+                },
+                coursesToSchedule: {
+                    fallCourses: [],
+                    springCourses: [],
+                    summerCourses: []
+                },
+                professors: []
+            },
+          ]);
+          expect(response.body).toEqual("Error generating schedule(s).");
+          expect(response.statusCode).toEqual(400);
+      });
+    });
+  });
